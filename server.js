ef19291fcdda94f99d1444e60b8ea17f28c2c7ad
Victor M. Rivas <vrivas@ujaen.es>
2023-01-16 13:40:02 +0100

    Instalo expreess y añado fichero server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const routes = require("./routes");
+const app = express();
+const port = 8001;
+app.use("/", routes);
+
+app.listen(port, () => {
+  console.log(`Microservicio PERSONAS ejecutándose en puerto ${port}!`);
+});
+
+
+module.exports=app
