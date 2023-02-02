3fa0fe562a8db819c3f90dfbe935839c154c9edf
Victor M. Rivas <vrivas@ujaen.es>
2023-02-02 17:49:00 +0100

    Comentarios a const y funciones de callback.js

--- callbacks.js
+++ callbacks.js
@@ -1,6 +1,10 @@
-// callbacks.js - callbacks for microservicio-1.
-// CALLBACKS DEL MODELO
-// CALLBACKS DEL MODELO
+// callbacks.js - callbacks para ms personas
+// Los callbacks son las funciones que se llaman cada vez que se recibe una petición a través de la API.
+// Las peticios se reciben en las rutas definidas en routes.js, pero se procesan aquí.
+
+
+
+// Necesario para conectar a la BBDD faunadb
 const faunadb = require('faunadb'),
     q = faunadb.query;
 
@@ -8,8 +12,12 @@ const client = new faunadb.Client({
     secret: 'fnAE6dR1GVAA1qiaRxaSZtbA7yGo6OpT2cB5NQnb',
 });
 
+// CALLBACKS DEL MODELO
 
-// Permitir CORS
+// Función que permite servir llamadas sin importar el origen:
+// CORS significa Cross-Origin Resource Sharing
+// Dado un objeto de tipo respuesta, le añade las cabeceras necesarias para realizar CROS
+// Devuelve el mismo objeto para concatenar llamadas al mismo
 function CORS(res) {
     res.header('Access-Control-Allow-Origin', '*')
         .header(
@@ -18,8 +26,11 @@ function CORS(res) {
         )
     return res;
 }
+
+
 // CALLBACKS PARA SELECTS DEL MODELO
 const CB_MODEL_SELECTS = {
+    // Prueba de conexión a la BBDD
     test_db: async (req, res) => {
         try {
             let personas = await client.query(
@@ -33,6 +44,8 @@ const CB_MODEL_SELECTS = {
             res.status(500).json({ error: error.description })
         }
     },
+
+    // Devuelve todas las personas de la BBDD
     getTodas: async (req, res) => {
         try {
             let personas = await client.query(
@@ -55,6 +68,8 @@ const CB_MODEL_SELECTS = {
 
 
 // CALLBACKS ADICIONALES
+
+// Otros callbacks. Sirven para comprobar que el servidor funciona correctamente.
 const CB_OTHERS = {
     home: async (req, res) => {
         try {
